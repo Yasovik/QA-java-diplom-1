d446bfee4422179ca9714297b3e3f6732f693e90
Alexandr Zakurdaev <azakyrdaev@gmail.com>
2023-04-19 00:04:26 +0300

    Диплом задание 1.1

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,22 +1,46 @@
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class BunTest {
-    Bun bun = new Bun("Кратерная булка", 100.00f);
+    private final String name;
+    private final float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBunData() {
+        return new Object[][]{
+                {"Кратерная булка", 100f},
+                {"", Float.MAX_VALUE},
+                {null, 100f},
+                {"Кратерная булка Кратерная булка Кратерная булка Кратерная булка Кратерная булка", Float.MIN_VALUE},
+                {"$Кр@терн@я#булка!", -1},
+                {"Кратерная булка", 0}
+        };
+    }
 
     @Test
     public void getNameReturnName() {
-        String expectedName = "Кратерная булка";
+        Bun bun = new Bun(name, price);
+        String expectedName = name;
         String actualName = bun.getName();
-        Assert.assertEquals(expectedName, actualName);
+        assertEquals(expectedName, actualName);
     }
 
     @Test
     public void getPriceReturnPrice() {
-        float expectedPrice = 100.00f;
+        Bun bun = new Bun(name, price);
+        float expectedPrice = price;
         float actualPrice = bun.getPrice();
-        Assert.assertEquals(expectedPrice, actualPrice, 0f);
+        assertEquals(expectedPrice, actualPrice, 0f);
     }
 
 }
